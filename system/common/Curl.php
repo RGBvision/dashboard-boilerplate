b0f470dde3964341c66d48c8f81bdb308b61c00b
RGBvision <rgbvision.net@gmail.com>
2022-09-14 04:23:52 +0300

    chore: Curl class update

--- system/common/Curl.php
+++ system/common/Curl.php
@@ -9,7 +9,7 @@
  * @author     Alex Graham <contact@rgbvision.net>
  * @copyright  Copyright 2017-2022, Alex Graham
  * @license    https://dashboard.rgbvision.net/license.txt MIT License
- * @version    2.7
+ * @version    3.0
  * @link       https://dashboard.rgbvision.net
  * @since      File available since Release 2.2
  */
@@ -17,54 +17,88 @@
 class Curl
 {
 
+    /**
+     * Perform GET request
+     *
+     * @param string $url request URL
+     * @param array $headers request headers
+     * @param array $params request parameters
+     * @return string
+     */
     public static function get(string $url, array $headers = [], array $params = []): string
     {
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($params));
+        curl_setopt($ch, CURLOPT_URL, $url . ($params ? '?' . http_build_query($params) : ''));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $response = curl_exec($ch);
         curl_close($ch);
-        
+
         return $response ?: '';
     }
 
-    public static function post(string $url, array $headers = [], array $params = []): string
+    /**
+     * Perform POST request
+     *
+     * @param string $url request URL
+     * @param array $headers request headers
+     * @param string $params request parameters
+     * @return string
+     */
+    private static function _post(string $url, array $headers = [], string $params = ''): string
     {
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $response = curl_exec($ch);
         curl_close($ch);
-        
+
         return $response ?: '';
     }
 
-    public static function postJson(string $url, array $headers = [], array $params = []): string
+    /**
+     * Perform POST request
+     *
+     * @param string $url request URL
+     * @param array $headers request headers
+     * @param array $params request parameters
+     * @return string
+     */
+    public static function post(string $url, array $headers = [], array $params = []): string
     {
-        $response = '';
-        try {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params, JSON_THROW_ON_ERROR));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            $response = curl_exec($ch);
-            curl_close($ch);
-        } catch (JsonException $e) {
-            return '';
-        }
+        return self::_post($url, $headers, http_build_query($params));
+    }
 
-        return $response ?: '';
+    /**
+     * Perform GET request
+     *
+     * @param string $url request URL
+     * @param array $headers request headers
+     * @param array $params request parameters
+     * @return mixed
+     */
+    public static function getJson(string $url, array $headers = [], array $params = []): mixed
+    {
+        return Json::decode(self::get($url, [...$headers, 'Content-Type: application/json'], $params));
+    }
+
+    /**
+     * Perform POST request
+     *
+     * @param string $url request URL
+     * @param array $headers request headers
+     * @param array $params request parameters
+     * @return mixed
+     */
+    public static function postJson(string $url, array $headers = [], array $params = []): mixed
+    {
+        return Json::decode(self::_post($url, [...$headers, 'Content-Type: application/json'], Json::encode($params)));
     }
 
 }
